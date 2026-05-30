082f4246035e3c28653e50fcb7655ec4c22c64aa
liadriscoll-arch <liadriscoll@dunlapstudents.net>
2026-05-30 13:58:43 -0500

    a

--- Scripts/farmhouse.gd
+++ Scripts/farmhouse.gd
@@ -1,5 +1,9 @@
 extends AnimatedSprite2D
 
+@onready var farm_house: AnimatedSprite2D = $"."
+
+func _ready() -> void:
+	farm_house.play("default")
 
 func _on_body_entered(body):
 	if Input.is_action_just_pressed("ui_accept"):
